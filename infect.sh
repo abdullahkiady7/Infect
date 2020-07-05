d98e457046e47be709b91402a585648102f01191
Fahad Khalid <67527602+itxfahdi@users.noreply.github.com>
2020-07-05 15:38:50 +0500

    Update infect.sh

--- infect.sh
+++ infect.sh
@@ -26,25 +26,6 @@
 # ███░░░░▄▀░░░░███░░▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
 # █████░░░░░░█████░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█
 # ████████████████████████████████████████████████████████████████████████████
-
-CorrectUsername = "fahdi"
-CorrectPassword = "maha"
-
-
-loop = 'true'
-while (loop == 'true'):
-    username = raw_input("\033[1;96m[☆] \x1b[1;97mUSER ID \x1b[1;96m>>>> ")
-    if (username == CorrectUsername):
-    	password = raw_input("\033[1;96m[☆] \x1b[1;97mPASWORD \x1b[1;96m>>>> ")
-        if (password == CorrectPassword):
-            print "Logged in successfully as " + username
-            loop = 'false'
-        else:
-            print "Wrong Password"
-            os.system('xdg-open https://www.facebook.com/groups/itxfahdi')
-    else:
-        print "Wrong Username"
-        os.system('xdg-open https://www.facebook.com/groups/itxfahdi')
 		
 		
 # A tool by fahdi www.facebook.com/itx.fahdi
